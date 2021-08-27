8d38fb1173d6740ea42bcb7d096154bafcd4004e
Guilherme Bazzo <gmbazzo@gmail.com>
2021-08-27 20:37:39 -0300

    Adicionando metodo estatico de unidade federativas

---
+++ 01_a/unidade_federativa.py
@@ -0,0 +1,7 @@
+class Unidade_Federativa:
+    @staticmethod
+    def __init__():
+        __lista = ["Pará/PA", "Amapá/AP", "Ceará/CE", "Santa Catarina/SC"]
+
+    def get_lista(self, __lista):
+        return __lista
