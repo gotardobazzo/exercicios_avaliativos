aec39a39f00dae28e0d098ba484abae1b9d7668d
Guilherme Bazzo <gmbazzo@gmail.com>
2021-08-27 19:57:27 -0300

    Adicionando diagrama de soma estatica e a funcao estatica

---
+++ 01_b/main.py
@@ -0,0 +1,7 @@
+from soma_estatica import Soma
+
+numeros = [2, 5 ,7, 4, 6,]
+resultado = Soma.soma(numeros)
+print("Minha da idade atual é: "+ str(resultado))
+
+
---
+++ 01_b/soma_estatica.py
@@ -0,0 +1,8 @@
+class Soma:
+
+    @staticmethod
+    def soma(numeros):
+        resultado = 0
+        for numero in numeros:
+            resultado = resultado + numero
+        return resultado
