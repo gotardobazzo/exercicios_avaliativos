2d0e88681544f9ea79dc0b3f7b62ee2f3d2ece37
Seu Nome <exemplo@seuemail.com.br>
2021-08-27 21:57:19 -0300

    Adicionando um laço for para enumerar os elementos da lista

--- 01_a/main.py
+++ 01_a/main.py
@@ -1,4 +1,7 @@
 from unidade_federativa import Unidade_Federativa
 
 estados = Unidade_Federativa.get_lista_uf()
-print("Estados em que já morei: "+ str(estados))
+estados0 = Unidade_Federativa.get_uf()
+
+for indice, estado in enumerate(estados):
+    print(estados[0, 1])
--- 01_a/unidade_federativa.py
+++ 01_a/unidade_federativa.py
@@ -2,5 +2,15 @@ class Unidade_Federativa:
 
     @staticmethod
     def get_lista_uf():
-        vetor_uf = ["Santa Catarina/SC ", "Amapá/AP ", "Pará/PA ", "Ceará/CE"]
+        vetor_uf = ["Santa Catarina", "Amapá ", "Pará ", "Ceará"]
         return vetor_uf
+
+    @staticmethod
+    def get_uf():
+        vetor0_uf = ["SC", "AP ", "PA ", "CE"]
+        return vetor0_uf
+
+"""
+
+
+"""
