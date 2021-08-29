2750e1fd8c74c35549e9233cb24f8f3beec30129
Guilherme Bazzo <gmbazzo@gmail.com>
2021-08-29 20:34:42 -0300

    Ajustado os vetores de acordo com seus Estados

--- 01_a/unidade_federativa.py
+++ 01_a/unidade_federativa.py
@@ -2,5 +2,11 @@ class Unidade_Federativa:
 
     @staticmethod
     def get_lista_uf():
-        vetor_uf = ["Santa Catarina/SC ", "Amapá/AP ", "Pará/PA ", "Ceará/CE"]
+        vetor_estado = ["- Santa Catarina ", "- Amapá ", "- Pará ", "- Ceará"]
+        return vetor_estado
+
+    @staticmethod
+    def get_uf():
+        vetor_uf = ["SC", "AP", "PA", "CE"]
         return vetor_uf
+
