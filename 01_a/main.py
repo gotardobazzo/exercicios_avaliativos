c814976cfab58ac8717561976d0bf4926a5d172f
Guilherme Bazzo <gmbazzo@gmail.com>
2021-08-29 20:31:44 -0300

    Terminando o metodo que trás as uf pela escolha do usuario

--- 01_a/main.py
+++ 01_a/main.py
@@ -1,4 +1,20 @@
 from unidade_federativa import Unidade_Federativa
 
-estados = Unidade_Federativa.get_lista_uf()
-print("Estados em que já morei: "+ str(estados))
+unestados = Unidade_Federativa.get_lista_uf()
+unuf = Unidade_Federativa.get_uf()
+
+for counter, value in enumerate(unestados):
+    print(counter, value)
+
+for counter, value in enumerate(unuf):
+    sigla = input("Escolha a numeracao do estado que deseja saber a UF: ")
+    if (sigla == "0"):
+        print("A UF do estado selecionado é: " + unuf[0])
+    elif (sigla == "1"):
+        print("A UF do estado selecionado é: " + unuf[1])
+    elif (sigla == "2"):
+        print("A UF do estado selecionado é: " + unuf[2])
+    elif (sigla == "3"):
+        print("A UF do estado selecionado é: " + unuf[3])
+
+
