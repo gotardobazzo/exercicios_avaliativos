6fafc2390090d993167f4b108da8fb89ecc83403
Guilherme Bazzo <gmbazzo@gmail.com>
2021-08-29 22:05:20 -0300

    Subindo atualizacao/finalizacao do programa que mostra a uf dos estados solicitados

---
+++ 01_a/main.py
@@ -0,0 +1,22 @@
+from unidade_federativa import Unidade_Federativa
+
+unestados = Unidade_Federativa.get_lista_uf()
+unuf = Unidade_Federativa.get_uf()
+
+print("LISTA DE ESTADOS")
+for counter, value in enumerate(unestados):
+    print(counter, value)
+
+for counter, value in enumerate(unuf):
+    sigla = input("Escolha o Estado para descobrir a UF: ")
+    if (sigla == "0"):
+        print("A UF do Estado de " + unestados[0] + " é " + unuf[0])
+    elif (sigla == "1"):
+        print("A UF do Estado do " + unestados[1] + " é " + unuf[1])
+    elif (sigla == "2"):
+        print("A UF do Estado do " + unestados[2] + " é " + unuf[2])
+    elif (sigla == "3"):
+        print("A UF do Estado d0 " + unestados[3] + " é " + unuf[3])
+
+
+
---
+++ 01_a/unidade_federativa.py
@@ -0,0 +1,11 @@
+class Unidade_Federativa:
+
+    @staticmethod
+    def get_lista_uf():
+        vetor_estado = ["Santa Catarina", "Amapá", "Pará ", "Ceará"]
+        return vetor_estado
+
+    @staticmethod
+    def get_uf():
+        vetor_uf = ["SC", "AP ", "PA ", "CE"]
+        return vetor_uf
