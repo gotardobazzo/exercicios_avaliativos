5796a8f3bc21f867659b5bac1c97e5c75dbcb9ca
GMBazzo <89359044+GMBazzo@users.noreply.github.com>
2021-08-29 21:32:46 -0300

    Deletado main.py pois haviam conflitos resolvidos

--- 01_a/main.py
+++
@@ -1,7 +0,0 @@
-from unidade_federativa import Unidade_Federativa
-
-estados = Unidade_Federativa.get_lista_uf()
-estados0 = Unidade_Federativa.get_uf()
-
-for indice, estado in enumerate(estados):
-    print(estados[0, 1])
